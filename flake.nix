72e09f32172b1bb86d0f24cbd20bcfd8315a287d
Liza Belonina <fkme.fuhua@gmail.com>
2026-08-14 14:06:57 +0400

    Configure Hyprland

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		home-manager = {
-			url = "github:nix-community/home-manager";
+			url = "github:nix-community/home-manager/release-25.05";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
@@ -13,14 +13,6 @@
 			modules = [
 				./configuration.nix
 				./hardware-configuration.nix
-				home-manager.nixosModules.default {
-					home-manager = {
-						useGlobalPkgs = true;
-						useUserPackages = true;
-						extraSpecialArgs = { inherit inputs; };
-						users.yoel = ./home.nix;
-					};
-				}
 			];
 		};
 	};
--- home.nix
+++ home.nix
@@ -1,44 +1,107 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
 {
-	home.username = "yoel";
-	home.homeDirectory = "/home/yoel";
-	home.stateVersion = "24.11";
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "yoel";
+  home.homeDirectory = "/home/yoel";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "26.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/yoel/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
 	wayland.windowManager.hyprland = {
-		systemd.enable = false;
-		settings = {
-			general = {
-				gaps_in = 5;
-				gaps_out = 20;
-				resize_on_border = false;
-				allow_tearing = false;
-				layout = "dwindle";
-			};
-			decoration = {
-				rounding = 10;
-				active_opacity = "0.97";
-				inactive_opacity = "0.8";
-			};
-			input = {
-				kb_layout = "us,ru";
-				kb_options = "grp:win_space_toggle";
-			};
-			"$mod" = "SUPER";
-			"$terminal" = "kitty";
-			"$browser" = "firefox";
-			"$fileManager" = "nautilus";
-			exec-once = [
-				"noctalia"
-			];
-			bind = [
-				"$mod, Q, exec, $termanal"
-				"$mod, C, killactive, "
-				"$mod, M, exit, "
-				"$mod, E, exec, $fileManager"
-			];
-			bindm = [
-				"$mod, mouse:272, movewindow"
-				"$mod, mouse:273, resizewindow"
-			];
-		};
+		enable = true;
+		systemd.enable = true;
+		configType = "lua";
+		extraConfig = ''
+hl.on("hyprland.start", function() hl.exec_cmd("noctalia") end)
+hl.bind("SUPER + Q", hl.dsp.exec_cmd("kitty"))
+hl.bind("SUPER + C", hl.dsp.window.close())
+hl.bind("SUPER + E", hl.dsp.exec_cmd("nautilus"))
+hl.bind("SUPER + mouse:272", hl.dsp.window.drag(), { mouse = true })
+hl.bind("SUPER + mouse:273", hl.dsp.window.resize(), { mouse = true })
+
+hl.general = {
+	gaps_in = 5;
+	gaps_out = 20;
+	resize_on_border = false;
+	allow_tearing = false;
+	layout = "dwindle";
+}
+hl.decoration = {
+	rounding = 10;
+	active_opacity = 0.97;
+	inactive_opacity = 0.8;
+}
+hl.input = {
+	kb_layout = "us,ru";
+	kb_options = "grp:win_space_toggle";
+}
+		'';
 	};
 }
