e9dfe768b53d62c72bc6b5ae7400ceab0ce81cc3
Liza Belonina <fkme.fuhua@gmail.com>
2026-08-18 10:07:41 +0400

    Добавила файл packages.nix

---
+++ modules/packages.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }
+{
+  environment.systemPackages = with pkgs; [
+   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   wget
+   curl
+   git
+   kitty
+   firefox
+   telegram-desktop
+   zsh-powerlevel10k
+   curl
+   amnezia-vpn
+   steam
+   discord-ptb
+   amberol
+   btop
+   fastfetch
+   obsidian
+   nautilus
+   gimp
+   qbittorrent
+  ];
+  users.defaultUserShell = pkgs.zsh;
+}
